e3d11ba15a44bcf0f1720f23f10524720a5b5635
Nikolai Doroshinsky <61108556+Sand-by@users.noreply.github.com>
2022-01-15 16:25:59 +0300

    Delete folder from main

--- js/sketch.js
+++ js/sketch.js
@@ -38,7 +38,6 @@ const startButton = gui.add( obj, 'Start' ); 	// button
 const stopButton = gui.add( obj, 'Stop' ).disable(); 	// button
 const folder2 = gui.addFolder( 'Lowpass' );
 folder2.add(obj,'NumberOfSamples',1,50,1);
-const folder3 = gui.addFolder('SomeFolder');
 
 init();
 animate();
